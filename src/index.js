950eb8cbee1411678d5eebb377dd9c58f2b4f5b8
Frostyy <woody7maddox@yahoo.dk>
2020-09-17 13:59:19 +0200

    So far so.. 'good'. Balls n shit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import Paddle from '/src/paddle';
+import Paddle from "./paddle.js";
+import InputHandler from "./input.js";
 
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext('2d');
 
-
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-
-
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+new InputHandler(paddle);
 
 paddle.draw(ctx);
 
-let lasTime = 0;
+let lastTime = 0;
+
+let imgBall = document.getElementById('img_ball');
 
 function gameLoop(timestamp){
     let deltaTime = timestamp - lastTime;
@@ -23,6 +25,8 @@ function gameLoop(timestamp){
     ctx.clearRect(0, 0, 800, 600);
     paddle.update(deltaTime);
     paddle.draw(ctx);
+
+    ctx.drawImage(imgBall, 16, 10, 16, 16);
 
     requestAnimationFrame(gameLoop);
 }
